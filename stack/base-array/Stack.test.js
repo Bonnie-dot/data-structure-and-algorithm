d20e80d5d6bae35d6c8d7560dd40f40ac4fd0e24
Qing Chen <q.chen@rea-group.com>
2021-02-28 13:34:27 +0800

    [stackBaseArray] catch error when stack overflow

--- stack/base-array/Stack.test.js
+++ stack/base-array/Stack.test.js
@@ -4,11 +4,11 @@ import Stack from "./Stack";
 describe('test stack with related operation',()=>{
 
     test('should correctly push when add new value',()=>{
-        const stack = new Stack();
+        const stack = new Stack(2);
         stack.push(1);
         stack.push(2);
         const result = stack.getValues();
-        expect(result).toEqual([2,1])
+        expect(result).toEqual([1,2])
     });
 
     test('should correctly pop when remove new value',()=>{
@@ -19,4 +19,10 @@ describe('test stack with related operation',()=>{
         const result = stack.getValues();
         expect(result).toEqual([1])
     });
+
+    test('should throw new stackOverflowError when count is greater than size',()=>{
+        const stack = new Stack(1);
+        stack.push(1);
+        expect(()=>{stack.push(2)}).toThrow('stack overflow error')
+    })
 })
--- stack/base-array/Stack.ts
+++ stack/base-array/Stack.ts
@@ -1,15 +1,32 @@
 export default class Stack {
-    array = [];
+
+    array: Array<unknown>;
+    size: number;
+    count: number= -1;
+
+    constructor(size: number) {
+        this.size = size;
+        this.array = new Array(size);
+    }
+
 
     push(value: unknown) {
-        this.array.unshift(value);
+        this.count++;
+        if(this.size<=this.count){
+            throw new Error(`stack overflow error`);
+        }
+        this.array[this.count]=value;
     }
 
     getValues() {
         return this.array;
     }
 
-    pop(){
-        this.array.splice(0,1);
+    pop() {
+        this.count--;
+        if(this.count===-1){
+            return false
+        }
+        this.array[this.count-1]=undefined;
     }
 }
