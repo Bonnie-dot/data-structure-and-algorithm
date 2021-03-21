43dbf4b60bf4f8bd8b6a77e42f594864780eb17c
Qing Chen <q.chen@rea-group.com>
2021-03-21 14:34:51 +0800

    [queueBaseCircle] add enqueue and dequeue

---
+++ 03_queue/circle_queue_array/Queue.test.ts
@@ -0,0 +1,42 @@
+import Queue from "./Queue";
+
+describe('Queue operating', () => {
+
+    it('should enqueue successfully when call enqueue without full', function () {
+
+        const queue = new Queue(5);
+        queue.enqueue(1);
+
+        expect(queue.enqueue(2)).toEqual(true);
+        expect(queue.data).toEqual([1, 2, undefined, undefined, undefined]);
+    });
+
+    it('should enqueue fail when array is fill', function () {
+
+        const queue = new Queue(2);
+        queue.enqueue(1);
+        queue.enqueue(2);
+
+        expect(queue.enqueue(3)).toEqual(false);
+        // it cause spend a space in array.
+        expect(queue.data).toEqual([1,undefined]);
+    });
+
+    it('should dequeue successfully when call dequeue without full', function () {
+
+        const queue = new Queue(3);
+        queue.enqueue(1);
+        queue.enqueue(2);
+
+        expect(queue.dequeue()).toEqual(true);
+        expect(queue.data).toEqual([undefined,2,undefined]);
+    });
+
+    it('should dequeue fail when array is empty', function () {
+
+        const queue = new Queue(2);
+
+        expect(queue.dequeue()).toEqual(false);
+        expect(queue.data).toEqual([undefined,undefined]);
+    });
+})
