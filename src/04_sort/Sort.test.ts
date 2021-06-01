33bb588bc0144fdb5a85dccbc80bcea42e3f794e
bonnie <277497288@qq.com>
2021-06-01 21:48:52 +0800

    [selectSort] update select sort

--- src/04_sort/Sort.test.ts
+++ src/04_sort/Sort.test.ts
@@ -23,6 +23,13 @@ describe('test sort correctly', () => {
         expect(result).toEqual([1, 2, 3, 4, 5, 6]);
     });
 
+    test('should sort correctly when call selectSort', () => {
+        const sort = new Sort([4, 5, 6, 3, 2, 1]);
+        const result = sort.selectSort();
+
+        expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
     test('should sort correctly when call mergeSort', () => {
         const sort = new Sort([4, 5, 6, 3, 2, 1]);
         const result = sort.mergeSort(sort.array);
--- src/04_sort/Sort.ts
+++ src/04_sort/Sort.ts
@@ -31,6 +31,26 @@ export default class Sort {
 
     insertionSort() {
         if (this.length <= 1) {
+            return null;
+        }
+
+        for (let i = 1; i < this.length; i++) {
+            let j = i - 1;
+            let value = this.array[i];
+            for (; j >= 0; j--) {
+                if (this.array[j] > value) {
+                    this.array[j + 1] = this.array[j];
+                } else {
+                    break;
+                }
+            }
+            this.array[j + 1] = value;
+        }
+        return this.array;
+    }
+
+    selectSort() {
+        if (this.length <= 1) {
             return this.array;
         }
 
