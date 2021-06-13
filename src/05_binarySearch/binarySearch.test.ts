13f02c23ad3c3e5229ee702ee7ddc52ef467165c
bonnie <277497288@qq.com>
2021-06-13 10:28:54 +0800

    [binary search] search first meet value

--- src/05_binarySearch/binarySearch.test.ts
+++ src/05_binarySearch/binarySearch.test.ts
@@ -1,13 +1,34 @@
-import {searchValueWithLoop, searchValueWithRecursive} from "./binarySearch";
+import {
+  searchValueWithFirstValue,
+  searchValueWithLoop,
+  searchValueWithRecursive,
+} from "./binarySearch";
+import { describe } from "@jest/globals";
 
-describe('should look up correctly',()=>{
-    it('should look up correctly when call searchValueWithLoop method', function () {
-        const result = searchValueWithLoop(19,[8,19,23,27,33,45,55,67,98]);
-        expect(result).toEqual(1);
-    });
+describe("should look up correctly", () => {
+  it("should look up correctly when call searchValueWithLoop method", function () {
+    const result = searchValueWithLoop(19, [8, 19, 23, 27, 33, 45, 55, 67, 98]);
+    expect(result).toEqual(1);
+  });
 
-    it('should look up correctly when call searchValueWithRecursive method', function () {
-        const result = searchValueWithRecursive(19,[8,19,23,27,33,45,55,67,98],0,9);
-        expect(result).toEqual(1);
-    });
-})
+  it("should look up correctly when call searchValueWithRecursive method", function () {
+    const result = searchValueWithRecursive(
+      19,
+      [8, 19, 23, 27, 33, 45, 55, 67, 98],
+      0,
+      9
+    );
+    expect(result).toEqual(1);
+  });
+
+  it("should look up first equal value when searchValueWithFirstValue method ", function () {
+    const result = searchValueWithFirstValue(
+      [8, 19, 19, 27, 33, 45, 55, 67, 98],
+      19,
+        0,
+        9
+    );
+    expect(result).toEqual(1);
+  });
+
+});
--- src/05_binarySearch/binarySearch.ts
+++ src/05_binarySearch/binarySearch.ts
@@ -1,31 +1,64 @@
-export const searchValueWithLoop = (searchValue, data):number=>{
-    let low: number = data[0];
-    let high: number = data.length-1;
-    while (low<=high){
-        let mid = Math.floor(low+(high - low)/2);
-        if (data[mid]===searchValue){
-            return mid
-        }else if (data[mid]>searchValue){
-            low = 0;
-            high = mid-1;
-        }else {
-            low = mid+1;
-            high = data.length -1;
-        }
+export const searchValueWithLoop = (searchValue, data): number => {
+  let low: number = data[0];
+  let high: number = data.length - 1;
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+    if (data[mid] === searchValue) {
+      return mid;
+    } else if (data[mid] > searchValue) {
+      low = 0;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+      high = data.length - 1;
     }
-    return -1;
-}
+  }
+  return -1;
+};
 
-export const searchValueWithRecursive = (searchValue, data:Array<number>,low:number,high:number):number=>{
-    let mid = Math.floor(low+(high - low)/2);
-    if (low>high){
-        return -1
-    }
-    if (data[mid]===searchValue){
-        return mid
-    }else if (data[mid]>searchValue){
-        return searchValueWithRecursive(searchValue,data,0,mid-1)
-    }else {
-        return searchValueWithRecursive(searchValue,data,mid+1,data.length -1);
-    }
-}
+export const searchValueWithRecursive = (
+  searchValue,
+  data: Array<number>,
+  low: number,
+  high: number
+): number => {
+  let mid = Math.floor(low + (high - low) / 2);
+  if (low > high) {
+    return -1;
+  }
+  if (data[mid] === searchValue) {
+    return mid;
+  } else if (data[mid] > searchValue) {
+    return searchValueWithRecursive(searchValue, data, 0, mid - 1);
+  } else {
+    return searchValueWithRecursive(
+      searchValue,
+      data,
+      mid + 1,
+      data.length - 1
+    );
+  }
+};
+
+export const searchValueWithFirstValue = (
+  data: Array<number>,
+  searchValue: number,
+  low: number,
+  high: number,
+) => {
+  if (low>=high) {
+    return -1;
+  }
+  let mid = Math.floor(low + (high - low) / 2);
+  if (data[mid]<searchValue){
+    return searchValueWithFirstValue(data,searchValue,mid+1,high);
+  }else if (data[mid]>searchValue){
+    return searchValueWithFirstValue(data,searchValue,low,mid-1);
+  }else {
+   if (mid ===0 || data[mid -1] !== searchValue){
+     return mid;
+   }
+    return searchValueWithFirstValue(data,searchValue,low,mid-1);
+  }
+};
+
