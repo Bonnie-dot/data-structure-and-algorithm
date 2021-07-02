a66dc652ae27e773245bcfecb2bbfc36deea39da
bonnie <277497288@qq.com>
2021-07-02 17:29:57 +0800

    [heap] insert delete heap top

--- src/08_heap/Heap.test.ts
+++ src/08_heap/Heap.test.ts
@@ -1,39 +1,51 @@
-import { Heap } from './Heap'
+import { Heap } from './Heap';
 
 describe('heap operator', () => {
-  it('should insert correct when call insert', function () {
-    const heap = new Heap()
-    heap.insert(1)
-    const array = heap.print()
-    expect(array).toEqual([1])
-  })
+  it('should insert correct when call insert', () => {
+    const heap = new Heap();
+    heap.insert(1);
+    const array = heap.print();
+    expect(array).toEqual([1]);
+  });
 
-  it('should insert two values by order when call insert', function () {
-    const heap = new Heap()
-    heap.insert(1)
-    heap.insert(2)
-    const array = heap.print()
-    expect(array).toEqual([2, 1])
-  })
+  it('should insert two values by order when call insert', () => {
+    const heap = new Heap();
+    heap.insert(1);
+    heap.insert(2);
+    const array = heap.print();
+    expect(array).toEqual([2, 1]);
+  });
 
-  it('should insert four values by order when call insert', function () {
-    const heap = new Heap()
-    heap.insert(1)
-    heap.insert(2)
-    heap.insert(6)
-    heap.insert(4)
-    const array = heap.print()
-    expect(array).toEqual([6, 4, 2, 1])
-  })
+  it('should insert four values by order when call insert', () => {
+    const heap = new Heap();
+    heap.insert(1);
+    heap.insert(2);
+    heap.insert(6);
+    heap.insert(4);
+    const array = heap.print();
+    expect(array).toEqual([6, 4, 2, 1]);
+  });
 
-  it('should insert multiple values by order when call insert', function () {
-    const heap = new Heap()
-    heap.insert(1)
-    heap.insert(2)
-    heap.insert(6)
-    heap.insert(4)
-    heap.insert(3)
-    const array = heap.print()
-    expect(array).toEqual([6, 4, 2, 1, 3])
-  })
-})
+  it('should insert multiple values by order when call insert', () => {
+    const heap = new Heap();
+    heap.insert(1);
+    heap.insert(2);
+    heap.insert(6);
+    heap.insert(4);
+    heap.insert(3);
+    const array = heap.print();
+    expect(array).toEqual([6, 4, 2, 1, 3]);
+  });
+
+  it('should delete top values when call deleteHeapTop', () => {
+    const heap = new Heap();
+    heap.insert(1);
+    heap.insert(2);
+    heap.insert(6);
+    heap.insert(4);
+    heap.insert(3);
+    heap.deleteHeapTop();
+    const array = heap.print();
+    expect(array).toEqual([4, 3, 2, 1]);
+  });
+});
--- src/08_heap/Heap.ts
+++ src/08_heap/Heap.ts
@@ -2,22 +2,34 @@ export class Heap {
   private array: number[] = [null];
 
   insert (value: number) {
-    this.array.push(value)
-    let i = this.array.length - 1
-    const parent = Math.floor(i / 2)
+    this.array.push(value);
+    let i = this.array.length - 1;
+    const parent = Math.floor(i / 2);
     while (parent > 0 && this.array[i] > this.array[parent]) {
-      this.swap(i, parent)
-      i = parent
+      this.swap(i, parent);
+      i = parent;
+    }
+  }
+
+  deleteHeapTop () {
+    const count = this.array.length - 1;
+    this.array[1] = this.array[count];
+    this.array.splice(count, 1);
+    let i = 1;
+    const child = Math.floor(i * 2);
+    while (i < count && this.array[i] < this.array[child]) {
+      this.swap(i, child);
+      i = child;
     }
   }
 
   print () {
-    return this.array.slice(1)
+    return this.array.slice(1);
   }
 
   swap (number1: number, number2: number) {
-    const temp = this.array[number1]
-    this.array[number1] = this.array[number2]
-    this.array[number2] = temp
+    const temp = this.array[number1];
+    this.array[number1] = this.array[number2];
+    this.array[number2] = temp;
   }
 }
