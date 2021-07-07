ed9d0752c977de7685db63b39c858149013ce4d7
bonnie <277497288@qq.com>
2021-07-07 16:15:03 +0800

    [graph] add method breadthFirstSearch

--- src/09_graph/graph.test.ts
+++ src/09_graph/graph.test.ts
@@ -8,4 +8,21 @@ describe('graph', () => {
     expect(graph.adjacencyMatrix[0]).toEqual([1]);
     expect(graph.adjacencyMatrix[1]).toEqual([0]);
   });
+
+  it('should breadth first search when call breadthFirstSearch ', function () {
+    const graph = new Graph(8);
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 3);
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 4);
+    graph.addEdge(2, 5);
+    graph.addEdge(3, 4);
+    graph.addEdge(4, 5);
+    graph.addEdge(4, 6);
+    graph.addEdge(5, 7);
+    graph.addEdge(6, 7);
+    graph.breadthFirstSearch(0, 6);
+
+    expect(graph.path).toEqual('0146');
+  });
 });
--- src/09_graph/graph.ts
+++ src/09_graph/graph.ts
@@ -1,6 +1,9 @@
 export class Graph {
     vertexCount: number;
+    // 邻接表存储
     adjacencyMatrix: number[][] = [];
+    // 最短路径
+    path:string = '';
 
     constructor (vertexCount: number) {
       this.vertexCount = vertexCount;
@@ -12,5 +15,42 @@ export class Graph {
     addEdge (s: number, t: number) {
       this.adjacencyMatrix[s].push(t);
       this.adjacencyMatrix[t].push(s);
+    }
+
+    breadthFirstSearch (s:number, t:number) {
+      if (s === t) {
+        return false;
+      }
+      const visited:boolean[] = new Array(this.vertexCount);
+      const queue: number[] = [];
+      const prev = new Array(this.vertexCount);
+      visited[s] = true;
+      queue.push(s);
+      for (let i = 0; i < this.vertexCount; i++) {
+        prev[i] = -1;
+      }
+
+      while (queue.length !== 0) {
+        const w = queue.shift();
+        for (let i = 0; i < this.adjacencyMatrix[w].length; ++i) {
+          const q = this.adjacencyMatrix[w][i];
+          if (!visited[q]) {
+            prev[q] = w;
+            if (q === t) {
+              this.print(prev, s, t);
+              return;
+            }
+            visited[q] = true;
+            queue.push(q);
+          }
+        }
+      }
+    }
+
+    print (prev:number[], s:number, t:number) {
+      if (prev[t] !== -1 && t !== s) {
+        this.print(prev, s, prev[t]);
+      }
+      this.path += t.toString();
     }
 }
